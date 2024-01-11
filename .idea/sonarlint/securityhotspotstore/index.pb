456744734ffc5d1b0d4269059045b9cc9f84756e
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2024-01-12 06:03:08 +0700

    Init new project to repository.

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+D
+app/build.gradle.kts,d/2/d2a4383452aad84a18429a5844d31330a012a9e6
+s
+Capp/src/main/java/com/taufik/androidmachinelearning/MainActivity.kt,a/5/a596f5bd9e5d01c8751888ef15f616f4ef7176bc
---
+++ app/src/main/java/com/taufik/androidmachinelearning/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.taufik.androidmachinelearning
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.taufik.androidmachinelearning.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
