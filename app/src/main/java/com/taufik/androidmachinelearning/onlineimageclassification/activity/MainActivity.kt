780d94560e3e88233be36bfc719be05f3a2693b1
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2024-01-24 18:30:02 +0700

    Update: add new activities.

--- app/src/main/java/com/taufik/androidmachinelearning/onlineimageclassification/activity/MainActivity.kt
+++ app/src/main/java/com/taufik/androidmachinelearning/onlineimageclassification/activity/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.taufik.androidmachinelearning
+package com.taufik.androidmachinelearning.onlineimageclassification.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
