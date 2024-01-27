4f8e9d88cec8afb9a1660f10aa06632d66c5aa68
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2024-01-27 11:59:47 +0700

    Update: add function to upload image.

--- app/src/main/java/com/taufik/androidmachinelearning/onlineimageclassification/ui/activity/MainImageClassificationActivity.kt
+++ app/src/main/java/com/taufik/androidmachinelearning/onlineimageclassification/ui/activity/MainImageClassificationActivity.kt
@@ -1,4 +1,4 @@
-package com.taufik.androidmachinelearning.onlineimageclassification.activity
+package com.taufik.androidmachinelearning.onlineimageclassification.ui.activity
 
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -10,11 +10,22 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.net.toUri
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
+import com.google.gson.Gson
 import com.taufik.androidmachinelearning.databinding.ActivityMainImageClassificationBinding
-import com.taufik.androidmachinelearning.onlineimageclassification.activity.CameraImageClassificationActivity.Companion.CAMERAX_RESULT
+import com.taufik.androidmachinelearning.onlineimageclassification.data.api.ApiConfig
+import com.taufik.androidmachinelearning.onlineimageclassification.data.model.response.FileUploadResponse
 import com.taufik.androidmachinelearning.onlineimageclassification.ext.Ext.showToast
+import com.taufik.androidmachinelearning.onlineimageclassification.ui.activity.CameraImageClassificationActivity.Companion.CAMERAX_RESULT
 import com.taufik.androidmachinelearning.onlineimageclassification.utils.Constants.REQUIRED_PERMISSION
 import com.taufik.androidmachinelearning.onlineimageclassification.utils.getImageUri
+import com.taufik.androidmachinelearning.onlineimageclassification.utils.uriToFile
+import kotlinx.coroutines.launch
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import retrofit2.HttpException
 
 class MainImageClassificationActivity : AppCompatActivity() {
 
@@ -95,8 +106,46 @@ class MainImageClassificationActivity : AppCompatActivity() {
     }
 
     private fun uploadImage() {
-        this.showToast("Fitur ini belum tersedia")
+        currentImageUri?.let { uri ->
+            val imageFile = uriToFile(uri, this)
+            Log.d("Image Classification File", "showImage: ${imageFile.path}")
+            showLoading(true)
+
+            val requestImageFile = imageFile.asRequestBody("image/jpeg".toMediaType())
+            val multipartBody = MultipartBody.Part.createFormData(
+                "photo",
+                imageFile.name,
+                requestImageFile
+            )
+
+            lifecycleScope.launch {
+                try {
+                    val apiService = ApiConfig.getApiService()
+                    val successResponse = apiService.uploadImage(multipartBody)
+                    with(successResponse.data) {
+                        binding.resultTextView.text = if (isAboveThreshold) {
+                            showToast(successResponse.message)
+                            String.format("%s with %.2f%%", result, confidenceScore)
+                        } else {
+                            showToast("Model is predicted successfully but under threshold.")
+                            String.format(
+                                "Please use the correct picture because  the confidence score is %.2f%%",
+                                confidenceScore
+                            )
+                        }
+                    }
+                    showLoading(false)
+                } catch (e: HttpException) {
+                    val errorBody = e.response()?.errorBody()?.string()
+                    val errorResponse = Gson().fromJson(errorBody, FileUploadResponse::class.java)
+                    showToast(errorResponse.message)
+                    showLoading(false)
+                }
+            }
+        }
     }
+
+    private fun showLoading(isLoading: Boolean) = binding.progressIndicator.isVisible == isLoading
 
     private fun showImage() {
         currentImageUri?.let {
