961356f8c2e09acebdd0bb0b41bf333392bc2ecd
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2024-02-02 14:53:31 +0700

    Update: update code starter project.

--- app/src/main/java/com/taufik/androidmachinelearning/imageclassification/ui/activity/ResultImageClassificationTFLiteActivity.kt
+++ app/src/main/java/com/taufik/androidmachinelearning/imageclassification/ui/activity/ResultImageClassificationTFLiteActivity.kt
@@ -1,6 +1,8 @@
 package com.taufik.androidmachinelearning.imageclassification.ui.activity
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.taufik.androidmachinelearning.databinding.ActivityResultImageClassificationTfliteBinding
 
@@ -15,5 +17,18 @@ class ResultImageClassificationTFLiteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        getData()
+    }
+
+    private fun getData() {
+        val imageUri = Uri.parse(intent.getStringExtra(EXTRA_IMAGE_URI))
+        imageUri?.let {
+            Log.d("Image URI", "showImage: $it")
+            binding.resultImage.setImageURI(it)
+        }
+    }
+
+    companion object {
+        const val EXTRA_IMAGE_URI = "extra_image_uri"
     }
 }
