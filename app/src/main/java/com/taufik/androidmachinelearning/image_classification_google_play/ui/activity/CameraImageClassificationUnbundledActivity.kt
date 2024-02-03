134d408a18c58f2af976b8a6f9e682913b032126
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2024-02-03 10:18:09 +0700

    Update: update import.

--- app/src/main/java/com/taufik/androidmachinelearning/image_classification_google_play/ui/activity/CameraImageClassificationUnbundledActivity.kt
+++ app/src/main/java/com/taufik/androidmachinelearning/image_classification_google_play/ui/activity/CameraImageClassificationUnbundledActivity.kt
@@ -14,10 +14,10 @@ import androidx.camera.core.resolutionselector.ResolutionSelector
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import com.taufik.androidmachinelearning.databinding.ActivityCameraImageClassificationUnbundledBinding
-import com.taufik.androidmachinelearning.helper.ImageClassifierHelper
+import com.taufik.androidmachinelearning.image_classification_google_play.ui.helper.ImageClassifierHelper
 import com.taufik.androidmachinelearning.onlineimageclassification.ext.Ext.showToast
 import com.taufik.androidmachinelearning.utils.Constants
-import org.tensorflow.lite.task.vision.classifier.Classifications
+import org.tensorflow.lite.task.gms.vision.classifier.Classifications
 import java.text.NumberFormat
 import java.util.concurrent.Executors
 
@@ -50,6 +50,7 @@ class CameraImageClassificationUnbundledActivity : AppCompatActivity() {
                         showToast(error)
                     }
                 }
+
                 override fun onResults(results: List<Classifications>?, inferenceTime: Long) {
                     runOnUiThread {
                         results?.let { it ->
