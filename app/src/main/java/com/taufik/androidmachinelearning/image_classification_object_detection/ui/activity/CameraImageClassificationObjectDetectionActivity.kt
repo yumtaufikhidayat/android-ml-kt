800c84fb289c5bf90f944c4fe34ef9f560bf1605
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2024-02-04 06:01:37 +0700

    Update: add starter project.

---
+++ app/src/main/java/com/taufik/androidmachinelearning/image_classification_object_detection/ui/activity/CameraImageClassificationObjectDetectionActivity.kt
@@ -0,0 +1,129 @@
+package com.taufik.androidmachinelearning.image_classification_object_detection.ui.activity
+
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.core.resolutionselector.AspectRatioStrategy
+import androidx.camera.core.resolutionselector.ResolutionSelector
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.taufik.androidmachinelearning.databinding.ActivityCameraImageClassificationObjectDetectionBinding
+import com.taufik.androidmachinelearning.image_classification_object_detection.helper.ImageClassifierHelper
+import com.taufik.androidmachinelearning.onlineimageclassification.ext.Ext.showToast
+import com.taufik.androidmachinelearning.utils.Constants
+import org.tensorflow.lite.task.gms.vision.classifier.Classifications
+import java.text.NumberFormat
+import java.util.concurrent.Executors
+
+class CameraImageClassificationObjectDetectionActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityCameraImageClassificationObjectDetectionBinding.inflate(layoutInflater)
+    }
+    private var cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+    private lateinit var imageClassifierHelper: ImageClassifierHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        hideSystemUI()
+        startCamera()
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        imageClassifierHelper = ImageClassifierHelper(
+            context = this,
+            classifierListener = object : ImageClassifierHelper.ClassifierListener {
+                override fun onError(error: String) {
+                    runOnUiThread {
+                        showToast(error)
+                    }
+                }
+
+                override fun onResults(results: List<Classifications>?, inferenceTime: Long) {
+                    runOnUiThread {
+                        results?.let { it ->
+                            if (it.isNotEmpty() && it[0].categories.isNotEmpty()) {
+                                println(it)
+                                val sortedCategories = it[0].categories.sortedByDescending { it?.score }
+                                val displayResult =
+                                    sortedCategories.joinToString("\n") {
+                                        "${it.label} " + NumberFormat.getPercentInstance()
+                                            .format(it.score).trim()
+                                    }
+                                binding.tvResult.text = displayResult
+                                binding.tvInferenceTime.text = "$inferenceTime ms"
+                            } else {
+                                binding.tvResult.text = ""
+                                binding.tvInferenceTime.text = ""
+                            }
+                        }
+                    }
+                }
+            }
+        )
+
+        cameraProviderFuture.addListener({
+            val resolutionSelector = ResolutionSelector.Builder()
+                .setAspectRatioStrategy(AspectRatioStrategy.RATIO_16_9_FALLBACK_AUTO_STRATEGY)
+                .build()
+            val imageAnalyzer = ImageAnalysis.Builder()
+                .setResolutionSelector(resolutionSelector)
+                .setTargetRotation(binding.viewFinder.display.rotation)
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
+                .build()
+                .also {
+                    it.setAnalyzer(Executors.newSingleThreadExecutor()) { image ->
+                        imageClassifierHelper.classifyImage(image)
+                    }
+                }
+
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder().build().also {
+                it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+            }
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    this,
+                    cameraSelector,
+                    preview,
+                    imageAnalyzer
+                )
+            } catch (exc: Exception) {
+                showToast("Gagal memunculkan kamera.")
+                Log.e(Constants.TAG_CAMERA_ACTIVITY, "startCamera: ${exc.message}")
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun hideSystemUI() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+    companion object {
+        const val EXTRA_CAMERAX_IMAGE = "CameraX Image"
+        const val CAMERAX_RESULT = 200
+    }
+}
---
+++ app/src/main/java/com/taufik/androidmachinelearning/image_classification_object_detection/ui/activity/ResultImageClassificationObjectDetectionActivity.kt
@@ -0,0 +1,33 @@
+package com.taufik.androidmachinelearning.image_classification_object_detection.ui.activity
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.taufik.androidmachinelearning.R
+import com.taufik.androidmachinelearning.databinding.ActivityResultImageClassificationObjectDetectionBinding
+
+class ResultImageClassificationObjectDetectionActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityResultImageClassificationObjectDetectionBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        getData()
+    }
+
+    private fun getData() {
+        val imageUri = Uri.parse(intent.getStringExtra(EXTRA_IMAGE_URI))
+        imageUri?.let {
+            Log.d("Image URI", "showImage: $it")
+            binding.resultImage.setImageURI(it)
+        }
+    }
+
+    companion object {
+        const val EXTRA_IMAGE_URI = "extra_image_uri"
+    }
+}
