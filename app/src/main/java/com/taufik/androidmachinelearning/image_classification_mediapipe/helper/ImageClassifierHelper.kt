3ec0f36013e4218bd401fc406f9f1b908883fb88
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2024-02-05 06:10:39 +0700

    Update: add MediaPipe import and update logic code for MediaPipe.

--- app/src/main/java/com/taufik/androidmachinelearning/image_classification_mediapipe/helper/ImageClassifierHelper.kt
+++ app/src/main/java/com/taufik/androidmachinelearning/image_classification_mediapipe/helper/ImageClassifierHelper.kt
@@ -6,21 +6,24 @@ import android.os.SystemClock
 import android.util.Log
 import android.view.Surface
 import androidx.camera.core.ImageProxy
+import com.google.mediapipe.framework.image.BitmapImageBuilder
 import com.taufik.androidmachinelearning.R
 import org.tensorflow.lite.DataType
 import org.tensorflow.lite.support.common.ops.CastOp
 import org.tensorflow.lite.support.image.ImageProcessor
 import org.tensorflow.lite.support.image.TensorImage
 import org.tensorflow.lite.support.image.ops.ResizeOp
-import org.tensorflow.lite.task.core.BaseOptions
-import org.tensorflow.lite.task.core.vision.ImageProcessingOptions
-import org.tensorflow.lite.task.vision.classifier.Classifications
-import org.tensorflow.lite.task.vision.classifier.ImageClassifier
+import com.google.mediapipe.tasks.components.containers.Classifications
+import com.google.mediapipe.tasks.core.BaseOptions
+import com.google.mediapipe.tasks.vision.core.ImageProcessingOptions
+import com.google.mediapipe.tasks.vision.core.RunningMode
+import com.google.mediapipe.tasks.vision.imageclassifier.ImageClassifier
 
 class ImageClassifierHelper(
-    var threshold: Float = 0.1f,
-    var maxResults: Int = 3,
+    val threshold: Float = 0.1f,
+    val maxResults: Int = 3,
     val modelName: String = "mobilenet_v1.tflite",
+    val runningMode: RunningMode = RunningMode.LIVE_STREAM,
     val context: Context,
     val classifierListener: ClassifierListener?
 ) {
@@ -34,14 +37,29 @@ class ImageClassifierHelper(
         val optionsBuilder = ImageClassifier.ImageClassifierOptions.builder()
             .setScoreThreshold(threshold)
             .setMaxResults(maxResults)
+            .setRunningMode(runningMode)
+
+        if (runningMode == RunningMode.LIVE_STREAM) {
+            optionsBuilder.setResultListener { result, image ->
+                val finishTimeMs = SystemClock.uptimeMillis()
+                val inferenceTime = finishTimeMs - result.timestampMs()
+                classifierListener?.onResults(
+                    result.classificationResult().classifications(),
+                    inferenceTime
+                )
+            }
+            optionsBuilder.setErrorListener { error ->
+                classifierListener?.onError(error.message.toString())
+            }
+        }
+
         val baseOptionsBuilder = BaseOptions.builder()
-            .setNumThreads(4)
+            .setModelAssetPath(modelName)
         optionsBuilder.setBaseOptions(baseOptionsBuilder.build())
 
         try {
-            imageClassifier = ImageClassifier.createFromFileAndOptions(
+            imageClassifier = ImageClassifier.createFromOptions(
                 context,
-                modelName,
                 optionsBuilder.build()
             )
         } catch (e: IllegalStateException) {
@@ -51,27 +69,19 @@ class ImageClassifierHelper(
     }
 
     fun classifyImage(image: ImageProxy) {
+
         if (imageClassifier == null) {
             setupImageClassifier()
         }
 
-        val imageProcessor = ImageProcessor.Builder()
-            .add(ResizeOp(224, 224, ResizeOp.ResizeMethod.NEAREST_NEIGHBOR))
-            .add(CastOp(DataType.UINT8))
-            .build()
+        val mpImage = BitmapImageBuilder(toBitmap(image)).build()
 
-        val tensorImage = imageProcessor.process(TensorImage.fromBitmap(toBitmap(image)))
         val imageProcessingOptions = ImageProcessingOptions.builder()
-            .setOrientation(getOrientationFromRotation(image.imageInfo.rotationDegrees))
+            .setRotationDegrees(image.imageInfo.rotationDegrees)
             .build()
 
-        var inferenceTime = SystemClock.uptimeMillis()
-        val results = imageClassifier?.classify(tensorImage, imageProcessingOptions)
-        inferenceTime = SystemClock.uptimeMillis() - inferenceTime
-        classifierListener?.onResults(
-            results,
-            inferenceTime
-        )
+        val inferenceTime = SystemClock.uptimeMillis()
+        imageClassifier?.classifyAsync(mpImage, imageProcessingOptions, inferenceTime)
     }
 
     private fun toBitmap(image: ImageProxy): Bitmap {
@@ -83,15 +93,6 @@ class ImageClassifierHelper(
         image.use { bitmapBuffer.copyPixelsFromBuffer(image.planes[0].buffer) }
         image.close()
         return bitmapBuffer
-    }
-
-    private fun getOrientationFromRotation(rotation: Int): ImageProcessingOptions.Orientation {
-        return when (rotation) {
-            Surface.ROTATION_270 -> ImageProcessingOptions.Orientation.BOTTOM_RIGHT
-            Surface.ROTATION_180 -> ImageProcessingOptions.Orientation.RIGHT_BOTTOM
-            Surface.ROTATION_90 -> ImageProcessingOptions.Orientation.TOP_LEFT
-            else -> ImageProcessingOptions.Orientation.RIGHT_TOP
-        }
     }
 
     interface ClassifierListener {
