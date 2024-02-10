69b385b8ee67c1129c80310a2411a74212fbbb03
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2024-02-10 10:13:21 +0700

    Update: add chat fragment.

--- app/src/main/java/com/taufik/androidmachinelearning/smartreply_mlkit/ui/activity/MainSmartReplyActivity.kt
+++ app/src/main/java/com/taufik/androidmachinelearning/smartreply_mlkit/ui/activity/MainSmartReplyActivity.kt
@@ -1,8 +1,11 @@
 package com.taufik.androidmachinelearning.smartreply_mlkit.ui.activity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.taufik.androidmachinelearning.R
 import com.taufik.androidmachinelearning.databinding.ActivityMainSmartReplyBinding
+import com.taufik.androidmachinelearning.smartreply_mlkit.ui.fragment.ChatFragment
 
 class MainSmartReplyActivity : AppCompatActivity() {
 
@@ -13,5 +16,17 @@ class MainSmartReplyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val fragmentManager = supportFragmentManager
+        val chatFragment = ChatFragment()
+        val fragment = fragmentManager.findFragmentByTag(ChatFragment::class.java.simpleName)
+
+        if (fragment !is ChatFragment) {
+            Log.d("MyChatFragment", "Fragment Name :" + ChatFragment::class.java.simpleName)
+            fragmentManager
+                .beginTransaction()
+                .add(R.id.container, chatFragment, ChatFragment::class.java.simpleName)
+                .commit()
+        }
     }
 }
